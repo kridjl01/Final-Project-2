3c08498088aaa34d2daec840a5a8a0d5a3f5f971
ETCS\jkri8336 <kridjl01@students.ipfw.edu>
2016-11-02 12:46:16 -0400

    Started making functions for login page

--- Script/jk-script.js
+++ Script/jk-script.js
@@ -1,3 +1,44 @@
 /**
  * Created by Owner on 10/29/2016.
  */
+
+var UserNames = [];
+var UserDifficulty = [];
+function $(elementid){
+    return document.getElementById(elementid);
+}
+
+function CheckUserName(UserName){
+    var savedUserName=0;
+    var index =0;
+    while (savedUserName==0){
+        if (UserNames[index]==UserName){
+            savedUserName =UserNames[index];
+
+        }
+        else{index++}
+
+    }
+}
+
+function NewUserName(){
+    //Takes a new user name and add new cell to username array
+     var newUserName = $("txtUserName").value
+    var lengthArray = UserNames.length;
+    var nextCell = lengthArray +1;
+    UserNames[nextCell]=newUserName;
+}
+
+function PreviousUser(){
+    // finds the previous users name
+    var previousUserName = $("txtUserName").value
+    var index = 0;
+    var length = UserNames.length;
+    while (previousUserName !=UserNames[index]&&index<=length){
+    index++;
+
+    }
+    if (index>length){
+        alert ("User Name is incorrect Please inter correct username or select new user.")
+    }
+}
